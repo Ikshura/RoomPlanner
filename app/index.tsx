5f2db6458eae7fe5c8238499ec717fbf987cbfd4
Ikshura <ikshu.tachi@gmail.com>
2025-06-12 23:30:20 +0800

    Implemented basic routing and changed the app logo
    Signed-off-by: Ikshura <ikshu.tachi@gmail.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hi Madeera!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl text-blue-500 font-bold">Hi Madoora!</Text>
+      <Link href="/roomsetup"> Create new setup </Link>
     </View>
   );
 }
---
+++ app/roomsetup.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const roomsetup = () => {
+  return (
+    <View>
+      <Text>roomsetup</Text>
+    </View>
+  )
+}
+
+export default roomsetup
+
+const styles = StyleSheet.create({})
